e12c48aaec871c8d01426e9b7a1510e97cc88697
Vishrut Reddi <vishrutreddi@gmail.com>
2018-04-27 20:21:56 -0700

    Comments

--- src/services/task.js
+++ src/services/task.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose';
 const Task = mongoose.model('task');
 
+/**
+ * Creates and saves a task
+ * @param {*} props Properties related to task
+ */
 function createTask(props) {
     const task = new Task(props);
     return task.save();
 }
 
+/**
+ * Deletes a task with the specified ID. If the task is not
+ * found then it throws an error.
+ * @param {*} id ID of the task to delete
+ */
 function deleteTask(id) {
     return Task.findOne({ id }).then((task) => {
         if(task) {
